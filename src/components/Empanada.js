ddfbf9bf3c45fbcd52f04b9ee5dbd689a0fd94f8
Facundo Rodriguez <facurodri91@gmail.com>
2022-01-25 21:25:34 -0500

    added filler pics

---
+++ src/components/Empanada.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Empanada() {
+    return (
+        <h4>
+            This is an empanada
+            <img src={"https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=750&h=375&url=https%3A%2F%2Fassets.marthastewart.com%2Fstyles%2Fwmax-750%2Fd17%2Fempanadas-a100668%2Fempanadas-a100668_horiz.jpg%3Fitok%3DWbDB4jCT"} />
+        </h4>
+    )
+}
+
+export default Empanada
